dfcd5fb916a85186b21d2816097fc2c625a767cc
alu0101028163 <alu0101028163@ull.edu.es>
2019-06-12 21:01:50 +0100

    npm test added

--- test/lexer/test_examples.js
+++ test/lexer/test_examples.js
@@ -22,7 +22,10 @@ describe("Lexer Examples", function(){
   { type: 'number', value: 1, lineno: 4, offset: 8 },
   { type: 'semicolon', value: ';', lineno: 4, offset: 9 },
   { type: 'keyword', value: 'while', lineno: 5, offset: 3 },
-  { type: 'identifier', value: 'n', lineno: 5, offset: 9 },
+  { type: 'identifier', value: 'n', lineno: 5, offset: 9 },  
+  { type: 'comparison_op', value: '<', lineno: 5, offset: 11 },
+  { type: 'number', value: 3, lineno: 5, offset: 13 },
+  { type: 'keyword', value: 'do', lineno: 5, offset: 15 },
   { type: 'keyword', value: 'begin', lineno: 6, offset: 3 },
   { type: 'identifier', value: 'n', lineno: 7, offset: 6 },
   { type: 'assign_op', value: ':=', lineno: 7, offset: 8 },
@@ -37,10 +40,7 @@ describe("Lexer Examples", function(){
   { type: 'identifier', value: 'n', lineno: 8, offset: 15 },
   { type: 'semicolon', value: ';', lineno: 8, offset: 16 },
   { type: 'keyword', value: 'end', lineno: 9, offset: 3 },
-  { type: 'semicolon', value: ';', lineno: 9, offset: 6 },
-  { type: 'keyword', value: 'end', lineno: 10, offset: 0 },
-  { type: 'dot', value: '.', lineno: 10, offset: 3 } ]
-
+  { type: 'keyword', value: 'end', lineno: 10, offset: 0 }]
   lexer.tokenize_program_from_file("examples/pl0_1.pl").should.eql(token_arr);
 
   })
